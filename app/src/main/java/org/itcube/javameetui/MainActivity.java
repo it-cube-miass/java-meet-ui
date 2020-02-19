40d0e4578929d4bd19fdb15a2a93bf1d4c33420a
haschish <haschish@gmail.com>
2020-02-20 00:19:51 +0500

    init

---
+++ app/src/main/java/org/itcube/javameetui/MainActivity.java
@@ -0,0 +1,79 @@
+package org.itcube.javameetui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int value = 0;
+
+    private Button btnAdd;
+    private Button btnTake;
+    private Button btnGrow;
+    private Button btnShrink;
+    private Button btnHide;
+    private Button btnReset;
+    private TextView txtValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnTake = (Button) findViewById(R.id.btnTake);
+        btnGrow = (Button) findViewById(R.id.btnGrow);
+        btnShrink = (Button) findViewById(R.id.brnShrink);
+        btnHide = (Button) findViewById(R.id.btnHide);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        txtValue = (TextView) findViewById(R.id.txtValue);
+
+        btnAdd.setOnClickListener(this);
+        btnTake.setOnClickListener(this);
+        btnGrow.setOnClickListener(this);
+        btnShrink.setOnClickListener(this);
+        btnHide.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        float size;
+
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                value ++;
+                txtValue.setText("" + value);
+                break;
+            case R.id.btnTake:
+                value --;
+                txtValue.setText("" + value);
+                break;
+            case R.id.btnReset:
+                value = 0;
+                txtValue.setText("" + value);
+                break;
+            case R.id.btnGrow:
+                size = txtValue.getTextScaleX();
+                txtValue.setTextScaleX(size + 1);
+                break;
+            case R.id.brnShrink:
+                size = txtValue.getTextScaleX();
+                txtValue.setTextScaleX(size - 1);
+                break;
+            case R.id.btnHide:
+                if (txtValue.getVisibility() == View.VISIBLE) {
+                    txtValue.setVisibility(View.INVISIBLE);
+                    btnHide.setText("show");
+                } else {
+                    txtValue.setVisibility(View.VISIBLE);
+                    btnHide.setText("hide");
+                }
+                break;
+        }
+    }
+}
